1f4d04710d2341eab228696217b4f1334f789dd6
sharif1995 <s.cookriley@gmail.com>
2017-06-03 09:55:57 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 " use strict ";
 
-var display = document.getElementById("display");
+//var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
+function oneToTen (){
+	for(var count = 1; count <= 10; count++){
+		document.write(count + "<br>");
+	}
+	//display.innerHTML = "hello";
 }
+
